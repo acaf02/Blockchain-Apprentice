d76b55c4dab5e51915d96c57ffc910c33b929479
Ana Carolina <anacarol.alves1@outlook.com>
2023-03-06 12:34:27 -0300

    update on node.js folder

--- Node.js/GuiaPerguntas/index.js
+++ Node.js/GuiaPerguntas/index.js
@@ -1,15 +1,53 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const connection = require('./database/database');
+const Pergunta = require('./database/Pergunta');
 
+
+connection
+    .authenticate()
+    .then(() => {
+        console.log('conexão feita com banco de dados');
+    })
+    .catch ((msgErro) => {
+        console.log(msgErro);
+    })
+
+
+//Express usar EJS com view engine
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+//body parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//Rotas
 app.get('/',(req,res) => {
-    res.render('index')
+    Pergunta.findAll({raw: true}).then(perguntas =>{
+        console.log(perguntas)
+        res.render('index'),{
+            perguntas:perguntas
+        }
+    });
+    
 });
 
 app.get('/perguntar', (req,res) => {
     res.render('perguntar');
+})
+
+app.post('/salvarpergunta', (req, res) =>{
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+
+    Pergunta.create({
+        titulo: titulo,
+        descricao: descricao
+    }).then(() => {
+        res.redirect('/');
+    })
 })
 
 app.listen(8080, ()=> {
